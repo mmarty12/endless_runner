8042fe7bec7633aaf51fcebceeacfbef39ca4523
Masha Martyniuk <mmartyniuk004@gmail.com>
2024-12-04 18:17:21 +0200

    implemented moving trap

--- Assets/trap.cs
+++ Assets/trap.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class trap : MonoBehaviour
 {
-   private void OnTriggerEnter2D(Collider2D collision) {
+   protected virtual void OnTriggerEnter2D(Collider2D collision) {
         if (collision.GetComponent<Player>() != null) {
             collision.GetComponent<Player>().Damage();
         }
